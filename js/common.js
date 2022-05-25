26bd6d78ec41830e87a66009d62f87ae02e490ac
d9m0n4 <c2h8e9d4@gmail.com>
2022-05-25 16:56:41 +0300

    ajax

--- js/common.js
+++ js/common.js
@@ -10,3 +10,47 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+// $(document).ready(() => {
+//   $('.loader_inner').fadeOut();
+//   $('.loader').delay(200).fadeOut('slow');
+// });
+
+const add = $('.add__button');
+const inputBlock = $('.form__inputs-block');
+
+add.on('click', (e) => {
+  e.preventDefault();
+  $(`<div id="meters__input" class="input__group">
+  <div class="input__group-item">
+    <label>Показания ПУ</label>
+    <input name="meters1" type="text" />
+  </div>
+</div>`)
+    .fadeIn('slow')
+    .appendTo(inputBlock);
+});
+
+$('#metersForm').on('submit', function (e) {
+  const d = $(this).serialize();
+  console.log(d);
+  console.log();
+  e.preventDefault();
+  $.ajax({
+    method: 'POST',
+    url: '../server.php',
+    xhrFields: {
+      withCredentials: true,
+    },
+    crossDomain: true,
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json',
+    data: $(this).serialize(),
+    success: function (d) {
+      console.dir(d);
+    },
+    error: (e) => {
+      console.log(e);
+    },
+  });
+});
