adff0ff15b05efa0244f2eff9e02cfe9e05ebafd
d9m0n4 <c2h8e9d4@gmail.com>
2023-02-20 16:47:15 +0300

    services grid

--- js/common.js
+++ js/common.js
@@ -1,2 +1,15 @@
+window.addEventListener('DOMContentLoaded', function () {
 
+})
 
+const elements = document.querySelectorAll('.services__row-item')
+const descr = document.querySelectorAll('.item__description')
+
+elements.forEach(element => {
+    function toggleClassName(e) {
+        e.stopPropagation()
+       let elem = e.target.matches('.item__description')
+        console.log(elem)
+    }
+    element.addEventListener('click', toggleClassName)
+})
