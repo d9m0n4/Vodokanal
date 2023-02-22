2a15b92f93be1021fdc37164aae4a6d28d037612
d9m0n4 <c2h8e9d4@gmail.com>
2023-02-22 13:04:20 +0300

    tabs

--- js/common.js
+++ js/common.js
@@ -3,13 +3,27 @@ window.addEventListener('DOMContentLoaded', function () {
 })
 
 const elements = document.querySelectorAll('.services__row-item')
-const descr = document.querySelectorAll('.item__description')
+const de = document.querySelectorAll('.item__description')
 
 elements.forEach(element => {
     function toggleClassName(e) {
-        e.stopPropagation()
-       let elem = e.target.matches('.item__description')
-        console.log(elem)
+        let elem = e.currentTarget
+        elements.forEach(d => d.classList.remove('opened'))
+        elem.classList.toggle('opened')
+
     }
     element.addEventListener('click', toggleClassName)
 })
+
+// const header = document.querySelector('.header__main')
+//
+// window.addEventListener('scroll', function() {
+//     // console.log(header.getBoundingClientRect().top)
+//     console.log(window.scrollY > header.getBoundingClientRect().top)
+//     // if(window.scrollY > header.getBoundingClientRect().top) {
+//     //     header.classList.add('header__fixed')
+//     // } else {
+//     //     header.classList.remove('header__fixed')
+//     // }
+// })
+
