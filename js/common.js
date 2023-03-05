0c5d8b3837c8cc7c9acc731555ab6afc48abfb20
d9m0n4 <c2h8e9d4@gmail.com>
2023-03-05 22:54:14 +0300

    mediaQ

--- js/common.js
+++ js/common.js
@@ -1,29 +1,28 @@
 window.addEventListener('DOMContentLoaded', function () {
-    // const mapInit = () => {
-    //     const map = new ymaps.Map('ymap', {
-    //         center: [],
-    //         zoom: 15
-    //     })
-    // }
-    // ymaps.ready(mapInit)
+  // const mapInit = () => {
+  //     const map = new ymaps.Map('ymap', {
+  //         center: [],
+  //         zoom: 15
+  //     })
+  // }
+  // ymaps.ready(mapInit)
 
-    const toggleAccordion = (selector, control, activeClass) => {
-        const accordions = document.querySelectorAll(selector)
-        if (accordions.length > 0) {
-            accordions[0].classList.add(activeClass)
-            accordions.forEach(acc => {
-                acc.addEventListener('click', (e) => {
-                    const currentAccordion = e.currentTarget
-                    if (e.target.closest(control)) {
-                        currentAccordion.classList.toggle(activeClass)
-                    }
-                })
-
-            })
-        }
+  const toggleAccordion = (selector, control, activeClass) => {
+    const accordions = document.querySelectorAll(selector);
+    if (accordions.length > 0) {
+      accordions[0].classList.add(activeClass);
+      accordions.forEach((acc) => {
+        acc.addEventListener('click', (e) => {
+          const currentAccordion = e.currentTarget;
+          if (e.target.closest(control)) {
+            currentAccordion.classList.toggle(activeClass);
+          }
+        });
+      });
     }
-    toggleAccordion('.page__accordion', '.page__accordion-title', 'active')
-})
+  };
+  toggleAccordion('.page__accordion', '.page__accordion-title', 'active');
+});
 
 // const elements = document.querySelectorAll('.services__row-item')
 // const de = document.querySelectorAll('.item__description')
@@ -50,94 +49,88 @@ window.addEventListener('DOMContentLoaded', function () {
 //     // }
 // })
 
-
 const scrollToTopOfElement = (el, elementContainer) => {
-    if (el.parentNode.getBoundingClientRect().top < 0) {
-        elementContainer.scrollIntoView({block: 'start'})
-    }
+  if (el.parentNode.getBoundingClientRect().top < 0) {
+    elementContainer.scrollIntoView({ block: 'start' });
+  }
+};
+
+const serviceItems = document.querySelectorAll('.services__row-item');
+
+serviceItems.forEach((item) => {
+  if (item.hasAttribute('data-target')) {
+    item.addEventListener('click', (e) => {
+      const itemDataPath = item.getAttribute('data-target');
+      const modal = document.querySelector('.modal');
+      modal.classList.add('active');
+
+      showContent(itemDataPath, modal);
+    });
+  }
+});
+
+function showContent(path, modal) {
+  if (path) {
+    modal.classList.add('active');
+    // const wrapper = document.querySelector('.services')
+    document.querySelectorAll(`[data-box]`).forEach((i) => i.classList.remove('active'));
+    const currentContent = document.querySelector(`[data-box=${path}]`);
+    currentContent.classList.add('active');
+    closeContent(currentContent, modal);
+    // scrollToTopOfElement(currentContent, wrapper)
+  }
 }
-
-
-const serviceItems = document.querySelectorAll('.services__row-item')
-
-serviceItems.forEach(item => {
-    if (item.hasAttribute('data-target')) {
-        item.addEventListener('click', (e) => {
-            const itemDataPath = item.getAttribute('data-target')
-            const modal = document.querySelector('.modal')
-            modal.classList.add('active')
-
-            showContent(itemDataPath, modal)
-        })
-    }
-})
-
-function showContent (path, modal)  {
-    if (path) {
-        modal.classList.add('active')
-        // const wrapper = document.querySelector('.services')
-        document.querySelectorAll(`[data-box]`).forEach(i => i.classList.remove('active'))
-        const currentContent = document.querySelector(`[data-box=${path}]`)
-        currentContent.classList.add('active')
-        closeContent(currentContent, modal)
-        // scrollToTopOfElement(currentContent, wrapper)
-    }
-}
-
-const modalOverlayClick = (overlay, modalRoot) => {
-    document.querySelector(overlay).addEventListener('click', (e) => {
-        if (!e.target.closest('.modal__content-item' )) {
-            document.querySelector(modalRoot).classList.remove('active')
-        }
-    })
-}
-
-modalOverlayClick('.modal__overlay', '.modal')
 
 function closeContent(s, modal) {
-    s.querySelector('.close__btn').addEventListener('click', (e) => {
-        modal.classList.remove('active')
-        s.classList.remove('active')
-    })
+  s.querySelector('.close__btn').addEventListener('click', (e) => {
+    modal.classList.remove('active');
+    s.classList.remove('active');
+  });
 }
 
 const showModal = (control) => {
-    const controlButtons = document.querySelectorAll(`[${control}]`)
-    const modal = document.querySelector('.modal')
-    controlButtons.forEach(c => {
-        c.addEventListener('click', (e) => {
-            const currentTargetPath = e.currentTarget.getAttribute('data-target')
-            showContent(currentTargetPath, modal)
-        })
-    })
-}
+  const controlButtons = document.querySelectorAll(`[${control}]`);
+  const modal = document.querySelector('.modal');
+  controlButtons.forEach((c) => {
+    c.addEventListener('click', (e) => {
+      const currentTargetPath = e.currentTarget.getAttribute('data-target');
+      showContent(currentTargetPath, modal);
+    });
+  });
+};
 
-showModal('data-control')
+showModal('data-control');
 
 class ServicesInfo {
-    constructor(selector, options) {
-        this.selector = selector
-        this.options = options
-    }
+  constructor(selector, options) {
+    this.selector = selector;
+    this.options = options;
+  }
 
-    init() {
-        console.log(this)
-    }
+  init() {
+    console.log(this);
+  }
 
-    log() {
-        console.log(this.selector)
-    }
+  log() {
+    console.log(this.selector);
+  }
 }
 
-const si = new ServicesInfo('.services__wrapper')
+const si = new ServicesInfo('.services__wrapper');
 
-const mobileNav = document.querySelector('.mobile__navigation')
-const burgerBtn = document.querySelector('.mobile__burger')
-const menu = document.querySelector('.header__navigation-list').cloneNode(true)
+const mobileNav = document.querySelector('.mobile__navigation');
+const burgerBtn = document.querySelector('.mobile__burger');
+const menu = document.querySelector('.header__navigation-list').cloneNode(1);
+
+const header = document.querySelector('.header');
+const headerHeight = header.offsetHeight;
+document.querySelector(':root').style.setProperty('--header-height', `${headerHeight}px`);
 
 burgerBtn.addEventListener('click', (e) => {
-    const target = e.currentTarget
-    target.classList.toggle('active')
-    mobileNav.classList.toggle('open')
-    mobileNav.appendChild(menu)
-})
+  const target = e.currentTarget;
+  mobileNav.appendChild(menu);
+  mobileNav.style.height = header.scrollHeight;
+  document.body.classList.toggle('scroll--off');
+  target.classList.toggle('active');
+  mobileNav.classList.toggle('open');
+});
